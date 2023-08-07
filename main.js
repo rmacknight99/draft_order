afc2bb46e0ee5f5165831e1d590df89ce9b2c8e3
rmacknight99 <rmacknig@andrew.cmu.edu>
2023-08-07 13:37:49 -0400

    app

---
+++ main.js
@@ -0,0 +1,23 @@
+let teams;
+
+function generateOrder() {
+    fetch('teams.json')
+        .then(response => response.json())
+        .then(data => {
+            teams = data.teams;
+            teams.sort(() => Math.random() - 0.5); // Shuffle the array
+            localStorage.setItem('teams', JSON.stringify(teams));
+            window.location.href = "order.html";
+        });
+}
+
+function displayOrder() {
+    const teams = JSON.parse(localStorage.getItem('teams'));
+    const teamList = document.getElementById('team-list');
+
+    teams.forEach(team => {
+        const li = document.createElement('li');
+        li.textContent = team;
+        teamList.appendChild(li);
+    });
+}
